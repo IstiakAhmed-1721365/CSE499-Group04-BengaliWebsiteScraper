8a1222a048b6b6db4dbea77449d071110c060ad0
IstiakAhmed-1721365 <s.m.ahmed@northsouth.edu>
2021-06-30 23:38:34 +0600

    merged with database code

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ for tc in wcon:
     main_text = tc.text
 print(main_text)
 
-#test
-#test2
-#test3
+cluster  = MongoClient("mongodb+srv://istiak:12345@cluster0.lqy6z.mongodb.net/test?retryWrites=true&w=majority ") 
+db = cluster["test"] 
+collection = db["test"] 
+post = {"_id": 14, "content": main_text} try: 
+    collection.insert_one(post) 
+    print('Stored successfully') 
+except: 
+   print('an error occurred quotes were not stored to db. Try changing the ID number in post') 
